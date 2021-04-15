94d2dd580e21ccd75b626dab437da39abf976c9f
openista1 <planbplnacnow@gmail.com>
2021-04-15 23:23:27 +0900

    chapter9

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const logic = document.querySelector('#logic');
-logic.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const { age, member } = event.target.elements;
-    const inputAge = parseInt(age.value);
-    const isMember = member[0].checked;
-    let price = null;
+const ul = document.querySelector('#fizzbuzz');
 
-    if (inputAge <= 15) {
-        price = 800;
-    } else if (isMember) {
-        price = 1000;
+for (let i = 1; i <= 100; i++) {
+    let answer = null;
+    if (i % 15 == 0) {
+        answer = "FizzBuzz";
+    } else if (i % 5 == 0) {
+        answer = 'Buzz';
+    } else if (i % 3 == 0) {
+        answer = 'Fizz';
     } else {
-        price = 1800;
+        answer = i;
     }
-
-    const displayPrice = document.querySelector('#price');
-    displayPrice.textContent = `料金は${price}円です。`
-})
+    const li = document.createElement('li');
+    li.textContent = `${i}: ${answer}`;
+    ul.appendChild(li);
+}
