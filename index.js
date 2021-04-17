ba3b31f9fdaa00a6990f982e508f43cb5e9960c0
openista1 <planbplnacnow@gmail.com>
2021-04-17 23:13:37 +0900

    chapter11

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const ul = document.querySelector('#kana');
-const kana = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ'];
-for (let i = 0; i < kana.length; i++) {
-    for (let j = 0; j < kana.length; j++) {
-        let li = document.createElement('li');
-        li.textContent = `${kana[i]}${kana[j]}`;
-        ul.appendChild(li);
-    }
+function calculateArea(radius) {
+    const area = radius * radius * 3.14;
+    const p = document.createElement('p');
+    p.textContent = `半径${radius}cmの円の面積は${area}平方cmです`;
+    const div = document.querySelector('#area');
+    div.appendChild(p);
 }
+
+calculateArea(5);
+calculateArea(10);
+calculateArea(15);
