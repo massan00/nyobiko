1f55077db8ec261ffb97a9f0deeb4ff941baef79
openista1 <planbplnacnow@gmail.com>
2021-04-18 23:28:26 +0900

    カウント計測

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function calculateArea(radius) {
-    const area = radius * radius * 3.14;
-    const p = document.createElement('p');
-    p.textContent = `半径${radius}cmの円の面積は${area}平方cmです`;
-    const div = document.querySelector('#area');
-    div.appendChild(p);
+const counter = {
+    number: 0,
+    countUp: function () {
+        counter.number++;
+    },
+    countDown: function () {
+        counter.number--;
+    },
+    countReset: function () {
+        counter.number = 0;
+    }
 }
 
-calculateArea(5);
-calculateArea(10);
-calculateArea(15);
+const button = document.createElement('button');
+button.textContent = 'カウントアップ';
+
+const button2 = document.createElement('button');
+button2.textContent = 'カウントダウン';
+
+const button3 = document.createElement('button');
+button3.textContent = 'カウントリセット';
+
+const p = document.createElement('p');
+p.textContent = counter.number;
+
+
+const num = document.querySelector('#area');
+num.appendChild(p);
+num.appendChild(button);
+num.appendChild(button2);
+num.appendChild(button3);
+
+
+button.addEventListener('click', () => {
+    counter.countUp();
+    p.textContent = counter.number;
+});
+
+button2.addEventListener('click', () => {
+    counter.countDown();
+    p.textContent = counter.number;
+})
+
+button3.addEventListener('click', () => {
+    counter.countReset();
+    p.textContent = counter.number;
+})
